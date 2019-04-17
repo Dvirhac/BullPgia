3f25b09753d09cd878165347291e3c544b803fbe
Dvirhac <dvirhacohen@gmail.com>
2019-04-17 15:19:32 +0300

    finish complication

--- calculate.cpp
+++ calculate.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "calculate.hpp"
+#include "SmartGuesser.hpp"
+#include "DummyChoosers.hpp"
+#include "DummyGuessers.hpp"
 namespace bullpgia {
     string calculateBullAndPgia(string s1, string s2) {
         string st;
