20a1b0b79646502ed043cf8f4d6fce4deafb150e
Dvirhac <dvirhacohen@gmail.com>
2019-05-05 16:36:00 +0300

    hopefully last commit

--- calculate.hpp
+++ calculate.hpp
@@ -4,4 +4,6 @@ using namespace std;
 
 namespace bullpgia{
     string calculateBullAndPgia(string chooser, string guesser);
+    int countPgia(string& choice, string& guess);
+    int countAndRemoveBull(string& choice, string& guess);
 }
