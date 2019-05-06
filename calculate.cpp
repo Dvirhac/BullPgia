0ca40531ca351a1124bfa61d23812a4372f6e412
Dvirhac <dvirhacohen@gmail.com>
2019-05-06 14:55:40 +0300

    hopefully last commit

--- calculate.cpp
+++ calculate.cpp
@@ -12,55 +12,49 @@ namespace bullpgia {
         int pgia = 0;
         int check[10];
 
-        //if (chooser.length() == guesser.length()) {
-            for (int i = 0; i < 10; i++) {
-                check[i] = 0;
+        for (int i = 0; i < 10; i++) {
+            check[i] = 0;
+        }
+
+        for (int i = 0; i < chooser.length(); i++) {
+            if (chooser[i] == guesser[i]) {
+                bull++;
+                chooser.erase(i, 1);
+                guesser.erase(i, 1);
+                i--;
             }
+        }
 
-            for (int i = 0; i < chooser.length(); i++) {
-                if (chooser[i] == guesser[i]) {
-                    bull++;
-                    chooser.erase(i, 1);
-                    guesser.erase(i, 1);
-                    i--;
-                }
-            }
-
-            for (int i = 0; i < guesser.size(); i++) {
-                char tempChar = guesser[i];
-                int tempInt = (int) tempChar - 48;
-                check[tempInt]++;
-            }
+        for (int i = 0; i < guesser.size(); i++) {
+            char tempChar = guesser[i];
+            int tempInt = (int) tempChar - 48;
+            check[tempInt]++;
+        }
 
 
-            for (int i = 0; i < chooser.size(); i++) {
-                for (int j = 0; j < guesser.size(); j++) {
-                    if (chooser[i] == guesser[j]) { //check for hits
-                        char temp = chooser[i];
-                        int temp2 = (int) temp - 48;
-                        if (check[temp2] > 0) {
-                            pgia++;
-                            check[temp2]--;
-                            j = guesser.length();
-                            continue;
-                        }
-
-
+        for (int i = 0; i < chooser.size(); i++) {
+            for (int j = 0; j < guesser.size(); j++) {
+                if (chooser[i] == guesser[j]) { //check for hits
+                    char temp = chooser[i];
+                    int temp2 = (int) temp - 48;
+                    if (check[temp2] > 0) {
+                        pgia++;
+                        check[temp2]--;
+                        j = guesser.length();
+                        continue;
                     }
 
-                }
 
+                }
 
             }
 
-            result = to_string(bull) + "," + to_string(pgia);
+        }
 
-            return result;
-        //}
-       // else {
-           // cout << " the choose is : " + chooser + " \n the guess is : " + guesser << endl;
-            //return "not equal";
-        //}
+        result = to_string(bull) + "," + to_string(pgia);
+
+        return result;
+
     }
 
 }
